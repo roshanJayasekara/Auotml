834dd44f018239c58c9e6f21c99a1d1292eb8489
Roshan Jayasekara <roshansampathmc41@gmail.com>
2023-02-15 09:36:15 +0530

    Bug fixes and Confusion matrix

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ import os
  # for profiling the data
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
+import matplotlib.pyplot as plt
 
 # ML libraries
-from pycaret.classification import setup, compare_models, pull, save_model
+from pycaret.classification import setup, compare_models, pull, save_model,plot_model
 
 with st.sidebar:
     st.image("logo.png")
@@ -40,16 +41,21 @@ if choice=="ML":
         st.title("Machine learning Model")
         chosen_target = st.selectbox('Choose the Target Column', df.columns)
         if st.button('Run Modelling'):
-            setup(df,target=chosen_target)
-            setup_df=pull()
-            st.info("This is the Ml expriment settings")
-            st.dataframe(setup_df)
-            best_model=compare_models()
-            compare_df=pull()
-            st.info("This is the Ml model")
-            st.dataframe(compare_df)
-            best_model
-            save_model(best_model, "best_model")
+            try:
+                setup(df,target=chosen_target)
+                setup_df=pull()
+                st.info("This is the Ml expriment settings")
+                st.dataframe(setup_df)
+                best_model=compare_models()
+                compare_df=pull()
+                st.info("This is the Ml model")
+                st.dataframe(compare_df)
+                best_model
+                save_model(best_model, "best_model")
+                st.set_option('deprecation.showPyplotGlobalUse', False)
+                st.pyplot(plot_model(best_model, plot = 'confusion_matrix',display_format="streamlit",plot_kwargs = {'percent' : True}))
+            except ValueError as e:
+                 st. write(str(e))
         
     else:
         st.title("Upload your file for modeling")
